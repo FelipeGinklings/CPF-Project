52385a11c49ab0704013608ecc09355a6ef5a503
Felipe Ginklings <41814653+FelipeGinklings@users.noreply.github.com>
2024-03-22 02:18:49 -0300

    Removendo verificação desnecessária

--- main.cpp
+++ main.cpp
@@ -30,20 +30,20 @@ d3 %= 10 -> 8 // Acha o último digito do novo número
 int main() {
     int cpf;
 
-    cout << "Qual eh os primeiro 9 digitos do seu cpf? ";
+    cout << "Qual eh os primeiro digitos (9 a 8) do seu cpf? ";
     cin >> cpf;
 
-    // Digitos
-    int d10, d9, d8, d7, d6, d5, d4, d3, d2;
-    d2 = cpf % 10; // Primeiro digito
-    d3 = ((cpf-(cpf % 10)) / 10) % 10;// Segundo digito    
-    d4 = ((cpf-(cpf % E2)) / E2) % 10; // Terceiro digito    
-    d5 = ((cpf-(cpf % E3)) / E3) % 10; // Quarto digito    
-    d6 = ((cpf-(cpf % E4)) / E4) % 10; // Quinto digito    
-    d7 = ((cpf-(cpf % E5)) / E5) % 10; // Sexto digito    
-    d8 = ((cpf-(cpf % E6)) / E6) % 10; // Sétimo digito    
-    d9 = ((cpf-(cpf % E7)) / E7) % 10; // Oitavo digito
-    d10 = ((cpf-(cpf % E8)) / E8) % 10;  // Nono digito    
+    // Digitos da esquerda para direita
+    int d2, d3, d4, d5, d6, d7, d8, d9, d10;
+    d2 = cpf % 10;
+    d3 = ((cpf-(cpf % 10)) / 10) % 10;
+    d4 = ((cpf-(cpf % E2)) / E2) % 10;
+    d5 = ((cpf-(cpf % E3)) / E3) % 10;
+    d6 = ((cpf-(cpf % E4)) / E4) % 10;
+    d7 = ((cpf-(cpf % E5)) / E5) % 10;
+    d8 = ((cpf-(cpf % E6)) / E6) % 10;
+    d9 = ((cpf-(cpf % E7)) / E7) % 10;
+    d10 = ((cpf-(cpf % E8)) / E8) % 10; 
 
     // Para o programa se tiver menos que 8 digitos
     if (d2 == 0 || d3 == 0 || d4 == 0 || d5 == 0 || d6 == 0 || d7 == 0 || d8 == 0 || d9 == 0) {
@@ -54,11 +54,9 @@ int main() {
     // Primeiro digito verificador do CPF
     int firstVD;
     // Soma dos digitos
-    firstVD = (d2 * 2) + (d3 * 3) + (d4 * 4) + (d5 * 5);
-    firstVD += (d6 * 6) + (d7 * 7) + (d8 * 8) + (d9 * 9);
-    if (d10 != 0) { // Caso o cpf tenha somente 8 digitos
-        firstVD += (d10 * 10);
-    }
+    firstVD = (d2 * 2) + (d3 * 3) + (d4 * 4);
+    firstVD += (d5 * 5) + (d6 * 6) + (d7 * 7);
+    firstVD += (d8 * 8) + (d9 * 9) + (d10 * 10);
     // Resto da divisão
     firstVD %= 11;
     if (firstVD < 2) {
@@ -71,11 +69,9 @@ int main() {
     int secondVD;
     // Soma dos digitos
     secondVD = firstVD * 2;
-    secondVD += (d2 * 3) + (d3 * 4) + (d4 * 5) + (d5 * 6);
-    secondVD +=  + (d6 * 7) + (d7 * 8) + (d8 * 9) + (d9 * 10);
-    if (d10 != 0) { // Caso o cpf tenha somente 8 digitos
-        secondVD += (d10 * 11);
-    }
+    secondVD += (d2 * 3) + (d3 * 4) + (d4 * 5);
+    secondVD += (d5 * 6) + (d6 * 7) + (d7 * 8);
+    secondVD += (d8 * 9) + (d9 * 10) + (d10 * 10);
     // Resto da divisão
     secondVD %= 11;
     if (secondVD < 2) {
@@ -84,11 +80,9 @@ int main() {
         secondVD = 11 - secondVD;
     }
 
-    // Mostra o novo cpf 
-    if (d10 != 0) { // Caso o cpf tenha somente 8 digitos
-        cout << d10;
-    }
-    cout << d9 << d8 << ".";
+    // Mostra o novo cpf
+    cout << "Seu CPF com os dois digitos verificadores eh: ";
+    cout << d10 << d9 << d8 << ".";
     cout << d7 << d6 << d5 << ".";
     cout << d4 << d3 << d2 << "-";
     cout << firstVD << secondVD << endl;
